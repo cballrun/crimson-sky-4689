a3b132c8802bcf4f1b49d8dc9658eced007a63b0
Carter Ball <carter.k.ball@gmail.com>
2022-09-20 18:06:59 -0400

    Spec variable alteration and test setup for dish name and description

--- spec/features/dishes/show_spec.rb
+++ spec/features/dishes/show_spec.rb
@@ -6,23 +6,33 @@ RSpec.describe 'the dish show page' do
     @c2 = Chef.create!(name: "Jim Lahey")
     @c3 = Chef.create!(name: "Ricky LaFleur")
 
-    @d1 = Dish.create!(name: "Cheeseburger", description: "Randy's Cheeseburger", chef_id: @c1.id)
-    @d2 = Dish.create!(name: "Pizza", description: "Randy's Pizza", chef_id: @c1.id)
+    @cheeseburger = Dish.create!(name: "Cheeseburger", description: "Randy's Cheeseburger", chef_id: @c1.id)
+    @pizza = Dish.create!(name: "Pizza", description: "Randy's Pizza", chef_id: @c1.id)
 
-    @d3 = Dish.create!(name: "Old Thompson", description: "Jim's Old Tom", chef_id: @c2.id)
-    @d4 = Dish.create!(name: "Taaka", description: "Jim's Taaka", chef_id: @c2.id)
+    @old_tom = Dish.create!(name: "Old Thompson", description: "Jim's Old Tom", chef_id: @c2.id)
+    @taaka = Dish.create!(name: "Taaka", description: "Jim's Taaka", chef_id: @c2.id)
 
-    @d5 = Dish.create!(name: "Goldy", description: "Ricky's Goldfish", chef_id: @c3.id)
-    @d6 = Dish.create!(name: "Hockey Stick", description: "Ricky's Hockey Stick", chef_id: @c3.id)
+    @goldy = Dish.create!(name: "Goldy", description: "Ricky's Goldfish", chef_id: @c3.id)
+    @stick = Dish.create!(name: "Hockey Stick", description: "Ricky's Hockey Stick", chef_id: @c3.id)
 
-    @i1 = Ingredient.create!(name: "Cheese", calories: 100)
-    @i2 = Ingredient.create!(name: "Liquor", calories: 200)
-    @i3 = Ingredient.create!(name: "Glass", calories: 300)
-    @i4 = Ingredient.create!(name: "Trailer", calories: 400)
+    @cheese = Ingredient.create!(name: "Cheese", calories: 100)
+    @liquor = Ingredient.create!(name: "Liquor", calories: 200)
+    @glass = Ingredient.create!(name: "Glass", calories: 300)
+    @trailer = Ingredient.create!(name: "Trailer", calories: 400)
+
+    @burger_cheese = DishIngredients.create!(dish_id: @cheeseburger.id, ingredient_id: @cheese.id)
+    @burger_liquor = DishIngredients.create!(dish_id: @cheeseburger.id, ingredient_id: @liquor.id)
   end
 
   it 'exists' do
-    visit "/dishes/#{@d1.id}"
+    visit "/dishes/#{@cheeseburger.id}"
+  end
+
+  it 'shows the dishes name and description' do
+    visit "/dishes/#{@cheeseburger.id}"
+
+    expect(page).to have_content("Dish Name: Cheeseburger")
+    expect(page).to have_content("Dish Description: Randy's Cheeseburger")
   end
 
 

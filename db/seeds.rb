679854ddb74e30cadf16257283d612c38326ce09
Carter Ball <carter.k.ball@gmail.com>
2022-09-20 17:52:37 -0400

    Seed setup

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+c1 = Chef.create!(name: "Randy Bobandy")
+c2 = Chef.create!(name: "Jim Lahey")
+c3 = Chef.create!(name: "Ricky LaFleur")
+
+d1 = Dish.create!(name: "Cheeseburger", description: "Randy's Cheeseburger", chef_id: c1.id)
+d2 = Dish.create!(name: "Pizza", description: "Randy's Pizza", chef_id: c1.id)
+
+d3 = Dish.create!(name: "Old Thompson", description: "Jim's Old Tom", chef_id: c2.id)
+d4 = Dish.create!(name: "Taaka", description: "Jim's Taaka", chef_id: c2.id)
+
+d5 = Dish.create!(name: "Goldy", description: "Ricky's Goldfish", chef_id: c3.id)
+d6 = Dish.create!(name: "Hockey Stick", description: "Ricky's Hockey Stick", chef_id: c3.id)
+
+i1 = Ingredient.create!(name: "Cheese", calories: 100)
+i2 = Ingredient.create!(name: "Liquor", calories: 200)
+i3 = Ingredient.create!(name: "Glass", calories: 300)
+i4 = Ingredient.create!(name: "Trailer", calories: 400)
---
+++ spec/features/dishes/show_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe 'the dish show' do
+  before :each do
+    visit '/dishes/1'
+  end
+
+
+end
